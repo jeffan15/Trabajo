76e816ed396594d750b3ec653bd1baa2e990010f
jeffan15 <37373382+jeffan15@users.noreply.github.com>
2018-04-03 23:37:56 -0500

    nuevo trabajo

--- jeff.cpp
+++ jeff.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int a, b;
-    cin>>a;
-    cin>>b;
-    if(a>b){
-        cout<<"el numero mayor es:"<<a;
-        cout<<"el numero menor es:"<<b;
-    }
-    else{
-        cout<<"el numero mayor es:"<<b;
-        cout<<"el numero menor es:"<<a;
-    }
-}
+//1
 int main(){
     int a;
     cin>>a;
-    while(a>0);
-        int b;
-        int b=a%10;
-        int a=a/10;
-        cout<<b<<endl;
+    int b=18;
+    if(a>b){
+        cout<<"es mayor de edad:"<<a;
     }
+    else{
+        cout<<"es menor de edad:"<<a;
+    }
+}
+//2
+int main(){
+    int a;
+    cin>>a;
+    int b=0;
+    while(b<a){
+        b++;
+        cout<<b<<",";
+    }
+    }
+//3
 int main(){
     int a, b, c;
     cin>>a;
@@ -43,37 +44,34 @@ int main(){
     }
     cout<<(a+b+c)/3;
     }
+//4
 int main(){
     int x, y;
     cin>>x;
     cin>>y;
-    switch(x, y){
-    case 1:
-        if((x,y)%2){
+    if((x,y)%2){
             cout<<"son multiplos de 2";
         }
         else{
             cout<<"no son multiplos de 2";
             }
-        }
-        break;
-    case 2:
-        if(x%y)
-            cout"son multiplos entre si";
-        else
-            cout"no son multiplos";
-    default:
-        if((pow(x,2))=y)
-            cout"son iguales"
+
+    if(x%y){
+            cout"son multiplos entre si";}
+        else{
+            cout"no son multiplos";}
+    if((pow(x,2))=y){
+            cout"son iguales"}
         else{
             cout"no son iguales"
         }
 }
+//5
 int main(){
     int a=0,i,n;
     cin>>n;
-    for(i=1:i<(n+1);i++){
-        if(n%1==0){
+    for(i=1;i<(n+1);i++){
+        if(n%i==0){
             a++;
         }}
         if(a!=2){
@@ -81,37 +79,65 @@ int main(){
         }else{
             cout<<"si es primo";
             }
-    return 0;
     }
+//6
 int main(){
-    int a;
-    cin>>a;
-    int primos;
-    int d;
-    while(primos>a){
-        primos=1;
-        for(d=2;d<n;++d){
-            if(n%d==0){
-                primos=0;
-                break;
+    int n;
+    cin>>n;
+    int b=1;
+    int c=2;
+    int d=2;
+        while(c<=n){
+            if(c%d==0){
+                if(c==d){
+                    cout<<c<<",";
+                    b++;
                 }
+                d=2;
+                c++;
+            }
+            else{
+            d++;
             }
         }
     }
+//7
 int main(){
     int x;
     cin>>x;
-    while(x>0){
-        int y
+    while(x>1){
         int y=x%10;
         int x=x/10;
         cout<<x<<endl;
+    break;
         }
         }
+//8
 int main(){
-    cout<<"ingrese un numero de 5 digitos:";
-    int a;
-    cin>>a}
+    int n,a,a1,b,b1,c,c1,d,d1,e;
+    cout<<"ingrese un numero de 5 digitos:"<<endl;
+    cin>>n;
+    a=n/10000;
+    a1=n%10000;
+    b=n%10;
+    if(a!=b){
+        cout<<"no es palindrome";
+    }
+    else{
+        c=a1/1000;
+        c1=a1%1000;
+        d=c1/100;
+        d1=c1%100;
+        e=d1/10;
+        if(c==e){
+            cout<<"si es palindrome";
+        }
+        else{
+            cout<<"no es palindrome";
+        }
+    }
+    }
+//9
 int main(){
     int a;
         cout<<"Ingresa año"<<endl;
@@ -121,8 +147,8 @@ int main(){
         }else{
             cout<<"El anio "<<a<<" No es bisiesto ";
         }
-        return 0;
 }
+//10
 int main(){
 	int a=1;
 	int b=0;
