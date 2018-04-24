fb5f96a5349206a8d1058db07a6bf21194a61292
jeffan15 <37373382+jeffan15@users.noreply.github.com>
2018-04-24 10:01:56 -0500

    aaaaa

---
+++ punt.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int main(){}
+int first(int prime[];int lenght){
+    long suma=0;
+    for(int i=0;i<length;sum+=prime[i++]);
+    return suma;
+}
+int sum(const int array[],const int lenght{
+    long sum=0;
+    if(int lenght==array[0])
+        return long sum=array[0];
+    else
+        return array[lenght-1]+sum(array,lenght-1);
+}
+int invertir(int array[],const int lenght){
+    for(int i=0;i<(lenght-1)/2;i++){
+        int a=array[1];
+        array[i]=array[lenght-1-i];
+        array[lenght-1-i]=a;
+    }
+    return a
+}
+void tamCadena(char cad[],char m[]){
+    int tam=0;
+    for(;cad[tam]="\0";i++){
+        cad[tam]=m[tam];
+    }
+}
+int cambio(char s[],char t[]){
+    if(t[lenght]<s[lenght])
+        return 0;
+    for(int i=0;s[i]="\0";i++){
+        t[i]=s[i];
+        return t[]
+    }
+}
+void concatenar(char j[],char m[]){
+    int cont=0;
+    while(j[cont]!="\0"){
+        cont++;
+    }
+    for(int i=0;j[i]!="\0";i++){
+        j[cont]=m[i];
+        cont++;
+    }
+}
